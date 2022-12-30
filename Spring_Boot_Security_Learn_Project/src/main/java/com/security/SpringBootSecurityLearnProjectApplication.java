fdd659677f414cf1e4eb59862d305e3556485cfe
ZAHID GUL <zahidgul107@gmail.com>
2022-12-30 10:59:21 +0530

    Started Project

---
+++ Spring_Boot_Security_Learn_Project/src/main/java/com/security/SpringBootSecurityLearnProjectApplication.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityLearnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityLearnProjectApplication.class, args);
+	}
+
+}
