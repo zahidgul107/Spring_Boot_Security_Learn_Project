eb669e2f4e7c0749e96283f068bab58319c8fc83
ZAHID GUL <zahidgul107@gmail.com>
2023-01-10 10:45:15 +0530

    added model and form

--- Spring_Boot_Security_Learn_Project/src/main/java/com/security/models/User.java
+++ Spring_Boot_Security_Learn_Project/src/main/java/com/security/models/User.java
@@ -1,7 +1,12 @@
 package com.security.models;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+	@Id
 	String username;
 	String password;
 	String email;
